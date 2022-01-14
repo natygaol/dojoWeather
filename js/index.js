c8e6f02816d138f4deb912644e5289935680a36f
nataly garcia <naty_garcioli@hotmail.com>
2022-01-13 22:26:37 -0500

    remove footer listo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 function informeClima( elemento) {
   alert("Cargando informe meteorológico…");
 }
+
+let footer = document.querySelector("footer");
+console.log(footer);
+
+function s ( element ) {
+  footer.remove();
+}
